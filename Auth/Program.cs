9a3cea1f5685f55ae4369925ea983800e6937403
ckomalram <glaw14@gmail.com>
2023-03-31 11:43:14 -0500

    ++ setting de conn sql server

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Auth.Context;
+using Auth.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Conexion a sql server
+builder.Services.AddSqlServer<PersonContext>(builder.Configuration.GetConnectionString("cnPerson"));
 
 var app = builder.Build();
 
